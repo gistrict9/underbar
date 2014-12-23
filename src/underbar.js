77bb8509145c2341621494390e2e1f3f5dcbd50c
Mike Donahue <mtdonahue1@gmail.com>
2014-12-22 19:29:33 -0500

    Completed Map

--- src/underbar.js
+++ src/underbar.js
@@ -119,6 +119,7 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+
   };
 
 
@@ -127,6 +128,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var result = [];
+    _.each(collection, function(value) {
+      result.push(iterator(value));  
+    });
+    return result;
   };
 
   /*
